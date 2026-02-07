d1688f28d5ae9340f6e70c022f79a1e4a2994764
mkdugri-blog <polysigil@gmail.com>
2026-02-07 18:37:03 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+        <BrowserRouter basename="/polysigil">
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="/about" element={<About />} />
